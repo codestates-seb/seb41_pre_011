1a803a641eee2916d4a5dfd0f5b46b5b781b6940
lisia004 <chkei0405@gmail.com>
2022-12-21 00:12:28 +0900

    태그 스텁

--- server/src/main/java/com/codestates/seb41_pre_011/tag/entity/Tag.java
+++ server/src/main/java/com/codestates/seb41_pre_011/tag/entity/Tag.java
@@ -1,4 +1,16 @@
 package com.codestates.seb41_pre_011.tag.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Tag {
+    private String name;
+    private String description;
+    private int count;
 }
