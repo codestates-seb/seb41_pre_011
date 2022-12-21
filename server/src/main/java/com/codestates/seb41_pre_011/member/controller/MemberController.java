f71b69dda413e48f23eae1a45a4709749787bcfe
lisia004 <chkei0405@gmail.com>
2022-12-21 15:54:17 +0900

    멤버 스텁

--- server/src/main/java/com/codestates/seb41_pre_011/member/controller/MemberController.java
+++ server/src/main/java/com/codestates/seb41_pre_011/member/controller/MemberController.java
@@ -1,46 +1,66 @@
 package com.codestates.seb41_pre_011.member.controller;
 
-import com.codestates.seb41_pre_011.member.dto.MemberDto;
 import com.codestates.seb41_pre_011.member.dto.MemberPatchDto;
 import com.codestates.seb41_pre_011.member.dto.MemberPostDto;
+import com.codestates.seb41_pre_011.member.dto.MemberResponseDto;
 import com.codestates.seb41_pre_011.member.entity.Member;
+import com.codestates.seb41_pre_011.member.mapper.MemberMapper;
+import com.codestates.seb41_pre_011.member.service.MemberService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Positive;
-import java.net.URI;
-import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/member")
+@Validated
 public class MemberController {
+
+    private final MemberService memberService;
+    private final MemberMapper memberMapper;
+
+    public MemberController(MemberService memberService, MemberMapper memberMapper) {
+        this.memberService = memberService;
+        this.memberMapper = memberMapper;
+    }
+
+
     @PostMapping
-    public ResponseEntity postMember(@RequestBody MemberPostDto postDto) {
-        return ResponseEntity.created(URI.create("v1/member/1")).build();
+    public ResponseEntity postMember(@RequestBody MemberPostDto memberPostDto) {
+        Member member = memberMapper.memberPostDtoToMember(memberPostDto);
+        Member response = memberService.createMember(member);
+        return new ResponseEntity<>(memberMapper.memberToMemberResponseDto(response), HttpStatus.CREATED);
     }
 
     @PatchMapping("/{member-id}")
     public ResponseEntity patchMember(@PathVariable("member-id") @Positive int memberId,
-                                      @RequestBody MemberPatchDto patchDto) {
-        MemberDto.Response response = new MemberDto.Response(1,"kch@gmail.com" , "홍길동", "비밀번호");
-        return ResponseEntity.ok(response);
+                                      @RequestBody MemberPatchDto memberPatchDto) {
+        memberPatchDto.setMemberId(memberId);
+        Member response = memberService.updateMember(memberMapper.memberPatchDtoTomMember(memberPatchDto));
+        return new ResponseEntity<>(memberMapper.memberToMemberResponseDto(response), HttpStatus.OK);
     }
 
     @GetMapping("/{member-id}")
     public ResponseEntity getMember(@PathVariable("member-id") @Positive int memberId) {
-        MemberDto.Response response = new MemberDto.Response(1,"kch@gmail.com" , "홍길동", "비밀번호");
-        return ResponseEntity.ok(response);
+        Member response = memberService.findMember(memberId);
+        return new ResponseEntity<>(memberMapper.memberToMemberResponseDto(response), HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity getMembers() {
-        MemberDto.Response response1 = new MemberDto.Response(1,"kch@gmail.com" , "홍길동", "비밀번호");
-        MemberDto.Response response2 = new MemberDto.Response(2,"chk@gmail.com" , "김코딩", "번호비밀");
-        return ResponseEntity.ok(Arrays.asList(response1, response2));
+        List<Member> members = memberService.findMembers();
+        List<MemberResponseDto> response = members.stream().map(member -> memberMapper.memberToMemberResponseDto(member))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{member-id}")
     public ResponseEntity deleteMember(@PathVariable("member-id") @Positive int memberId) {
-        return ResponseEntity.noContent().build();
+        MemberService.deleteMember(memberId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- server/src/main/java/com/codestates/seb41_pre_011/member/dto/MemberPostDto.java
+++ server/src/main/java/com/codestates/seb41_pre_011/member/dto/MemberPostDto.java
@@ -2,9 +2,18 @@ package com.codestates.seb41_pre_011.member.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 public class MemberPostDto {
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank(message = "이름이 공백입니다.")
     private String name;
+
+    @NotBlank(message = "비밀번호가 공백입니다.")
     private String password;
 }
