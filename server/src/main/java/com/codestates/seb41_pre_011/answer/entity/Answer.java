43b5795442cf0299be0058a0f4276a4e5040546b
lisia004 <chkei0405@gmail.com>
2022-12-23 22:56:02 +0900

    feat - #41 : 엔서 레포지터리 생성 및 서비스 클래스 2차 구현

--- server/src/main/java/com/codestates/seb41_pre_011/answer/entity/Answer.java
+++ server/src/main/java/com/codestates/seb41_pre_011/answer/entity/Answer.java
@@ -5,14 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int answerId;
+    @Column(nullable = false)
     private String content;
     private LocalDateTime createdDate = LocalDateTime.now();
     private LocalDateTime modifiedDate = LocalDateTime.now();
