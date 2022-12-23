8ee8d6609c8459719401264fcf9bed1f7c3bd0df
[codemodel6] <[codemodel6@gmail.com]>
2022-12-23 19:35:41 +0900

    feat - #46 : User 더미데이터 제외한 더미데이터 완성
    User 더미데이터를 잘못 수정하여 추후 수정하겠습니다

---
+++ client/src/dummy/AnswerDummyData.js
@@ -0,0 +1,24 @@
+const AnswerDummyData = [
+  { tagId: 1, name: 'spring', description: '프로그램', count: 20 },
+  { tagId: 2, name: 'python', description: '프로그램', count: 19 },
+  { tagId: 3, name: 'python', description: '프로그램', count: 18 },
+  { tagId: 4, name: 'c#', description: '프로그램', count: 17 },
+  { tagId: 5, name: 'android', description: '프로그램', count: 16 },
+  { tagId: 6, name: 'android', description: '프로그램', count: 15 },
+  { tagId: 7, name: 'jquery', description: '프로그램', count: 14 },
+  { tagId: 8, name: 'c++', description: '프로그램', count: 13 },
+  { tagId: 9, name: 'c++', description: '프로그램', count: 12 },
+  { tagId: 10, name: 'ios', description: '프로그램', count: 11 },
+  { tagId: 11, name: 'mysql', description: '프로그램', count: 10 },
+  { tagId: 12, name: 'sql', description: '프로그램', count: 11 },
+  { tagId: 13, name: 'node.js', description: '프로그램', count: 11 },
+  { tagId: 14, name: 'reactjs', description: '프로그램', count: 11 },
+  { tagId: 15, name: 'arrays', description: '프로그램', count: 11 },
+  { tagId: 16, name: 'asp.net', description: '프로그램', count: 11 },
+  { tagId: 17, name: 'json', description: '프로그램', count: 11 },
+  { tagId: 18, name: 'json', description: '프로그램', count: 11 },
+  { tagId: 19, name: 'python-3.x', description: '프로그램', count: 11 },
+  { tagId: 20, name: 'django', description: '프로그램', count: 11 },
+];
+
+export default AnswerDummyData;
---
+++ client/src/dummy/MemberDummyData.js
@@ -0,0 +1,16 @@
+const UserDummyData = [
+  {
+    answerId: 1,
+    content: 'Test answer1',
+    createdDate: '2022-12-23T16:10:44.9856362',
+    modifiedDate: '2022-12-23T16:10:44.9856362',
+  },
+  {
+    answerId: 2,
+    content: 'Test answer2',
+    createdDate: '2022-12-23T16:10:44.9856362',
+    modifiedDate: '2022-12-23T16:10:44.9856362',
+  },
+];
+
+export default UserDummyData;
