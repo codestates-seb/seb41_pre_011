12fc8c57e046b16999001fed471fb5025c13e95a
Gyuddi <samll8539@naver.com>
2022-12-29 17:10:43 +0900

    feat - #76 : 요구 사항 정의서에 맞게 기능 수정

--- server/src/main/java/com/codestates/seb41_pre_011/tag/entity/TagAnswer.java
+++ server/src/main/java/com/codestates/seb41_pre_011/tag/entity/TagAnswer.java
@@ -1,6 +1,7 @@
 package com.codestates.seb41_pre_011.tag.entity;
 
 import com.codestates.seb41_pre_011.answer.entity.Answer;
+import com.codestates.seb41_pre_011.tag.entity.Tag;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,7 +16,6 @@ public class TagAnswer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int tagAnswer;
-
     @ManyToOne
     @JoinColumn(name = "TAG_ID")
     private Tag tag;
