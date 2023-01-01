fafd8100c4dc5b9299d62c2e8cb4c619479573d4
lisia004 <chkei0405@gmail.com>
2023-01-01 15:06:12 +0900

    feat - #100 : cors 에러

--- server/src/main/java/com/codestates/seb41_pre_011/filter/CorsFilter.java
+++ server/src/main/java/com/codestates/seb41_pre_011/filter/CorsFilter.java
@@ -24,7 +24,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
 
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "http://ec2-13-209-138-5.ap-northeast-2.compute.amazonaws.com:8080");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
