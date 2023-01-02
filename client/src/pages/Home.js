d25cb935718fa80918aa6ea5b2d263281906a491
codemodel6@gmail.com <codemodel6@gmail.com>
2023-01-02 23:31:37 +0900

    feat - #125 : 홈 화면 및 질문의 총 개수 완

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import TitleBasic from '../components/titleBasic/TitleBasic';
+import stone from '../assets/stone.jpg';
 
 const Wrapper = styled.div`
   width: 1100px;
@@ -8,11 +9,29 @@ const Wrapper = styled.div`
   margin: 0 auto;
   display: flex;
 `;
+const TitleDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+  width: 100%;
+`;
+const StoneImg = styled.img`
+  border-radius: 20%;
+  width: 400px;
+  display: block;
+  margin: 30px auto;
+`;
 
 const Home = () => {
   return (
     <Wrapper>
-      <TitleBasic>Stack Overflow</TitleBasic>
+      <TitleDiv>
+        <TitleBasic>Welcome to 11 stone 2 birds Stack Overflow!</TitleBasic>
+        <h1>You are free to post questions and answers</h1>
+        <h1>Have a good time :{')'}</h1>
+        <StoneImg src={stone} alt="돌맹이" />
+      </TitleDiv>
     </Wrapper>
   );
 };
