972dbcea01fc8b997f0e0467fae76d38719acbe3
Gyuddi <samll8539@naver.com>
2023-01-02 14:07:58 +0900

    feat - #117 : member get, patch 메서드 수정

--- server/src/main/java/com/codestates/seb41_pre_011/auth/handler/MemberAuthenticationSuccessHandler.java
+++ server/src/main/java/com/codestates/seb41_pre_011/auth/handler/MemberAuthenticationSuccessHandler.java
@@ -2,6 +2,7 @@ package com.codestates.seb41_pre_011.auth.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +11,13 @@ import java.io.IOException;
 
 @Slf4j
 public class MemberAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException {
         log.info("# Authenticated successfully!");
-        response.getWriter().print("test");
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        response.getWriter().print(userDetails.getUsername());
     }
 }
